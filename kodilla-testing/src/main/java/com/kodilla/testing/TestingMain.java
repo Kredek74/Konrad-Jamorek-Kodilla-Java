df044b53551f8728707de8233b11a381f3482396
Kredek74 <Krdek74@protonmail.com>
2020-07-19 14:45:32 +0200

    Module 6. Task 2.

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,24 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+
+        Calculator calculator = new Calculator();
+        int addResult = 12;
+        int substractResult = 8;
+
+        if (addResult ==calculator.additionAtoB(7,5)){
+            System.out.println("Method additionAtoB works");
+        } else {
+            System.out.println("Error of method additionAtoB");
+        }
+
+        if (substractResult == calculator.substractionAfromB(14, 6)){
+            System.out.println("Method substractionAfromB works");
+        } else {
+            System.out.println("Error of method substractionAfromB");
+        }
     }
 }
