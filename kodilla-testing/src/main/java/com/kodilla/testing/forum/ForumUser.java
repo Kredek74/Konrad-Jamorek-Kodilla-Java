199d839e804754ee695842c4866ef82fc98f7239
Kredek74 <Krdek74@protonmail.com>
2020-07-22 21:52:16 +0200

    Module 6.4 Exercise

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/ForumUser.java
@@ -0,0 +1,9 @@
+package com.kodilla.testing.forum;
+
+import java.util.ArrayList;
+
+public class ForumUser {
+    private String name;
+    private String realName;
+    private ArrayList<ForumPost> posts = new ArrayList<ForumPost>()
+}
