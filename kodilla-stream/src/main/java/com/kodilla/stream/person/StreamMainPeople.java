94bc4903703d23bb648e97e67f808699afb20a30
Kredek74 <Krdek74@protonmail.com>
2020-08-13 01:06:08 +0200

    Module 7.3 Exercise

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/person/StreamMainPeople.java
@@ -0,0 +1,12 @@
+package com.kodilla.stream.person;
+
+public class StreamMainPeople {
+    public static void main(String[] args) {
+        People.getList().stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.length() > 11)
+                .map(s -> s.substring(0, s.indexOf(' ') + 2) + ".")
+                .filter(s -> s.substring(0, 1).equals("M"))
+                .forEach(System.out::println);
+    }
+}
